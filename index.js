a12073e84be7fb01f06e119bb53c2e4b5a8576ea
keiji-temari <kgykgt@gmail.com>
2024-10-16 00:56:49 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// LINE Messaging API用の設定
+const line = require('@line/bot-sdk');
+const config = {
+  channelAccessToken: 'あなたのチャネルアクセストークン',
+  channelSecret: 'あなたのチャネルシークレット'
+};
+const client = new line.Client(config);
+
+// リクエストの解析
+app.use(bodyParser.json());
+
+// QRコードに対応するエンドポイント
+app.get('/qr1', (req, res) => {
+  const userId = 'ユーザーのLINE ID';  // ここにユーザーIDを取得するロジックが必要
+  client.pushMessage(userId, {
+    type: 'text',
+    text: 'スポット1をクリアしました！'
+  });
+  res.send('QR1 スキャン完了');
+});
+
+app.get('/qr2', (req, res) => {
+  const userId = 'ユーザーのLINE ID';
+  client.pushMessage(userId, {
+    type: 'text',
+    text: 'スポット2をクリアしました！'
+  });
+  res.send('QR2 スキャン完了');
+});
+
+// サーバーを起動
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`サーバーがポート${port}で起動しました！`);
+});
